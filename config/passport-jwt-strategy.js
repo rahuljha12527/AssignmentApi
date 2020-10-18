6516fb7bb538f186d00d2b5f5c1181bdd68dff4f
rahuljha661 <rahuljha12527@gmail.com>
2020-10-18 10:39:48 +0530

    create a controller for signup

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -4,4 +4,29 @@ const JWTStrategy=require('passport-jwt').Strategy;
 
 const ExtractJWT=require('passport-jwt').ExtractJwt;
 
-const User=require('');
+const User=require('../models/user');
+
+let opts={
+    jwtFromRequest:ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey:"userApi",
+};
+
+passport.use(
+    new JWTStrategy(opts,function(jwtPayLoad,done){
+        console.log(opts,jwtPayLoad);
+
+        User.findById(jwtPayLoad._id,function(req,res){
+            if(err){
+                console.log("Error in finding the user from JWT");
+                return;
+            }
+            if(user){
+                return done(null,user);
+            }else{
+                return done(null,false);
+            }
+        });
+    })
+);
+
+module.exports=passport;
---
+++ controllers/api/v1/userController.js
@@ -0,0 +1,44 @@
+const User=require("../../../models/user");
+const jwt=require("jsonwebtoken");
+const {use}=require('passport');
+
+module.exports.register=async function(req,res){
+    if(!req.body.name || !req.body.email || !req.body.password){
+        return res.json(200,{
+            message:"Please fill all the required filled",
+        })
+    }
+
+    try{
+      await User.findOne({
+          email:req.body.email,
+      },function(err,user){
+          if(err){
+              return res.json(400,{
+                  message:"Username already exist",
+              });
+          }
+
+          if(!user){
+              User.create({
+                  name:req.body.name,
+                  email:req.body.email,
+                  password:req.body.password
+              });
+              return res.json(200,{
+                  message:"registered successfully"
+              });
+          }else {
+              return res.json(400,{
+                  message:"Username already exist",
+              });
+          }
+      })
+    }catch(err){
+        return res.json(500,{
+            message:"Internal Server Error",
+        })
+    }
+};
+
+
